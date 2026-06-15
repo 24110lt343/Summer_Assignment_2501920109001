62f9d43cb66a47e18bd930113b8f0883bd69f67e
Akash Mishra <akashmishra.cdx@gmail.com>
2026-06-15 22:34:48 +0530

    Tasks of day_1 Week_3 Completed

--- Week_3/Day_1/CycleInLL.java
+++ Week_3/Day_1/CycleInLL.java
@@ -1,13 +1,14 @@
 
-class Solution {
-    public ListNode middleNode(ListNode head) {
+public class Solution {
+    public boolean hasCycle(ListNode head) {
         ListNode slow = head,fast = head;
         while(fast!=null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
-            
+            if(slow == fast) return true;
         }
-        return slow;
+        return false;
     }
 }
+
 //Time Complexity : O(n)
--- Week_3/Day_1/ReverseLL.java
+++ Week_3/Day_1/ReverseLL.java
@@ -11,3 +11,4 @@ class Solution {
         return prev;
     }
 }
+//Time Complexity : O(n)
