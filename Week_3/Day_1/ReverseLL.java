676d7d989a08eed410da4d530be71235596a42a2
Akash Mishra <akashmishra.cdx@gmail.com>
2026-06-15 21:20:48 +0530

    2 problem solved on day_1 week_3

---
+++ Week_3/Day_1/ReverseLL.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null,current = head,n = head;
+        while(n!=null){
+            n = current.next;
+            current.next = prev;
+            prev = current;
+            current = n;
+        }
+        return prev;
+    }
+}
